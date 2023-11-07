c5d6e5b4af102f122f30d3b1dd4e5ed66c7d8509
NatureLin0510 <122120023+NatureLin0510@users.noreply.github.com>
2023-11-07 22:23:59 +0800

    Add files via upload

---
+++ autogui.py
@@ -0,0 +1,135 @@
+import pyautogui
+import time
+
+# 模拟鼠标移动
+#pyautogui.moveTo(100, 100, duration=1)  # 将鼠标移动到屏幕坐标(100, 100)，持续时间1秒
+
+# 模拟鼠标点击
+#pyautogui.click()  # 单击鼠标左键
+
+# 模拟键盘输入
+#pyautogui.typewrite("Hello, World!", interval=0.1)  # 输入文本，每个字符之间间隔0.1秒
+
+# 模拟键盘热键
+#pyautogui.hotkey('ctrl', 'c')  # 模拟按下Ctrl+C复制文本
+#pyautogui.hotkey('ctrl', 'v')  # 模拟按下Ctrl+V粘贴文本
+#huamian = pyautogui.size()
+#print(huamian)
+
+def allreport():
+    pyautogui.rightClick(507,480)
+    pyautogui.click(566,602)
+    pyautogui.click(762,376)
+    pyautogui.click(756,416)
+    pyautogui.click(756,479)
+    pyautogui.click(756,527)
+    pyautogui.click(756,575)
+    pyautogui.click(756,613)
+    pyautogui.click(756,652)
+    pyautogui.click(962,775)#發送檢舉
+    #==============================
+    pyautogui.rightClick(507,510)
+    pyautogui.click(571,634)
+    pyautogui.click(760,371)
+    pyautogui.click(760,414)
+    pyautogui.click(760,474)
+    pyautogui.click(760,516)
+    pyautogui.click(760,560)
+    pyautogui.click(760,608)
+    pyautogui.click(760,650)
+    pyautogui.click(962,775)#發送檢舉
+    #================================
+    pyautogui.rightClick(495,539)
+    pyautogui.click(566,660)
+    pyautogui.click(760,371)
+    pyautogui.click(760,414)
+    pyautogui.click(760,474)
+    pyautogui.click(760,516)
+    pyautogui.click(760,560)
+    pyautogui.click(760,608)
+    pyautogui.click(760,650)
+    pyautogui.click(962,775)#發送檢舉
+    #===============================
+    pyautogui.rightClick(492,580)
+    pyautogui.click(552,713)
+    pyautogui.click(760,371)
+    pyautogui.click(760,414)
+    pyautogui.click(760,474)
+    pyautogui.click(760,516)
+    pyautogui.click(760,560)
+    pyautogui.click(760,608)
+    pyautogui.click(760,650)
+    pyautogui.click(962,775)#發送檢舉
+    #================================
+    pyautogui.rightClick(489,652)
+    pyautogui.click(540,777)
+    pyautogui.click(760,371)
+    pyautogui.click(760,414)
+    pyautogui.click(760,474)
+    pyautogui.click(760,516)
+    pyautogui.click(760,560)
+    pyautogui.click(760,608)
+    pyautogui.click(760,650)
+    pyautogui.click(962,775)#發送檢舉
+    #===============================
+    pyautogui.rightClick(496,692)
+    pyautogui.click(536,832)
+    pyautogui.click(760,371)
+    pyautogui.click(760,414)
+    pyautogui.click(760,474)
+    pyautogui.click(760,516)
+    pyautogui.click(760,560)
+    pyautogui.click(760,608)
+    pyautogui.click(760,650)
+    pyautogui.click(962,775)#發送檢舉
+    #===============================
+    pyautogui.rightClick(491,728)
+    pyautogui.click(531,848)
+    pyautogui.click(760,371)
+    pyautogui.click(760,414)
+    pyautogui.click(760,474)
+    pyautogui.click(760,516)
+    pyautogui.click(760,560)
+    pyautogui.click(760,608)
+    pyautogui.click(760,650)
+    pyautogui.click(962,775)#發送檢舉
+    #===============================
+    pyautogui.rightClick(485,762)
+    pyautogui.click(524,749)
+    pyautogui.click(760,371)
+    pyautogui.click(760,414)
+    pyautogui.click(760,474)
+    pyautogui.click(760,516)
+    pyautogui.click(760,560)
+    pyautogui.click(760,608)
+    pyautogui.click(760,650)
+    pyautogui.click(962,775)#發送檢舉
+    #===============================
+    pyautogui.rightClick(483,798)
+    pyautogui.click(545,786)
+    pyautogui.click(760,371)
+    pyautogui.click(760,414)
+    pyautogui.click(760,474)
+    pyautogui.click(760,516)
+    pyautogui.click(760,560)
+    pyautogui.click(760,608)
+    pyautogui.click(760,650)
+    pyautogui.click(962,775)#發送檢舉
+#===============================
+
+def cccv():
+    time.sleep(2)
+    x, y = pyautogui.position()
+    print("鼠标当前位置:", x, y)
+    time.sleep(2)
+    x, y = pyautogui.position()
+    print("鼠标当前位置:", x, y)
+#cccv()
+allreport()
+
+
+
+
+
+
+
